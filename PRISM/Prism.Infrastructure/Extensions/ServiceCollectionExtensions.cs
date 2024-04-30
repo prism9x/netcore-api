1b81a71f031a558d56326dade61c814f7a0fb2e4
Đinh Mạnh Tưởng <145707598+prism9x@users.noreply.github.com>
2024-04-30 23:50:14 +0700

    IServiceCollection Extensions Method

---
+++ PRISM/Prism.Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Prism.Infrastructure.Persistence;
+using Prism.Infrastructure.Seeders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prism.Infrastructure.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static void AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("PrismDb");
+            services.AddDbContext<PrismDbContext>(options => options.UseSqlServer(connectionString));
+
+            services.AddScoped<IProductSeeders, ProductSeeders>();
+        }
+    }
+}

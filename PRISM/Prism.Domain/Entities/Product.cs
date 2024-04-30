63a86b6755f117dea3dec893a5538d54f7e002ed
Đinh Mạnh Tưởng <145707598+prism9x@users.noreply.github.com>
2024-04-30 21:50:40 +0700

    Thêm 2 thực thể - Category - Product

---
+++ PRISM/Prism.Domain/Entities/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prism.Domain.Entities
+{
+    public class Product
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string Description { get; set; }
+        public Category Category { get; set; }
+
+    }
+}

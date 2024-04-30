508a21c53c264bf6b4155f0a7f58c2d4680b77f8
Đinh Mạnh Tưởng <145707598+prism9x@users.noreply.github.com>
2024-04-30 23:50:54 +0700

    - AddInfrastructure - Seed Data

--- PRISM/Prism.API/Program.cs
+++ PRISM/Prism.API/Program.cs
@@ -1,20 +1,30 @@
+using Prism.Infrastructure.Extensions;
+using Prism.Infrastructure.Seeders;
 
 namespace Prism.API
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+            builder.Services.AddInfrastructure(builder.Configuration);
+
+
 
             builder.Services.AddControllers();
+
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
             var app = builder.Build();
+
+            var scope = app.Services.CreateScope();
+            var seeder = scope.ServiceProvider.GetRequiredService<IProductSeeders>();
+            await seeder.Seed();
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
